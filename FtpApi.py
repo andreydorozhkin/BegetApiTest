62c5f8e45791b656770393cc32feee9522a6a584
Andrey Dorozhkin <andreydorozhkin.00@gmail.com>
2021-11-18 16:53:06 +0300

    create function add_ftp_access and refactor function get_list

--- FtpApi.py
+++ FtpApi.py
@@ -2,16 +2,40 @@ import requests
 import json
 
 
+site = "https://api.beget.com/api/ftp/"
+get_auth_and_format = {'output_format': 'json', 'passwd': 'R2dONbctqg', 'login': 'vhostbg2'}
+post_auth_and_format = {'login': 'vhostbg2', 'passwd': 'R2dONbctqg', 'output_format': 'json', 'input_format': 'json'}
+
+
 def print_json(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
 
-auth_and_format = {'output_format': 'json', 'passwd': 'R2dONbctqg', 'login': 'vhostbg2'}
-ftp_getList = requests.get(
-    "https://api.beget.com/api/ftp/getList", params=auth_and_format)
-# add_ftp = {'password'}
-# ftp_add = requests.post("https://api.beget.com/api/ftp/add")
+def get_list():
+    command = site + "getList"
+    ftp_list_response = requests.get(
+        command, params=get_auth_and_format)
+    parsed = ftp_list_response.json()
+    return parsed['answer']['result']
 
 
-print_json(ftp_getList.json())
+def add_ftp_access(suffix, homedir, password):
+    command = site + "add"
+    inp_data_dict = {'suffix': suffix, "homedir": homedir, "password": password}
+    params = post_auth_and_format.copy()
+    params['input_data'] = inp_data_dict
+    print(params)
+    # ftp_status = requests.get(command, params=post_auth_and_format)
+    # print(ftp_status.request.url)
+    # return ftp_status
+
+
+add_ftp_access("testapicode", "/test_for_code_api/public_html", "Qwerty123")
+
+
+# ftp_list = get_list()
+# for i in ftp_list:
+#    print(i['login'])
+
+
